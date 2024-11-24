a3a7915da6514972e04cfce22993384739774f36
Ryan McCarthy <ryanmccarthy667@gmail.com>
2024-11-25 10:35:21 +1300

    added function to read wifi config from a file

--- micro_controller_code/src/main.rs
+++ micro_controller_code/src/main.rs
@@ -2,11 +2,31 @@ use std::{thread::sleep, time::Duration};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::wifi::{EspWifi,Configuration,ClientConfiguration};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
-use heapless::String;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
 
 
+// struct to hold the wifi connection details 
+#[derive(Deserialize)]
+struct WifiConfig{
+    ssid: String,
+    password : String,
+}
+
+// function to read wifi details from file 
+fn read_wifi_config()->WifiConfig{
+    //open the file 
+    let mut file = File::open("/spiffs/wifi_config.json").expect("wifi config file should exist on device");
+    let mut buffer = String::new();
+    //read the file into a json string                                                   
+    file.read_to_string(&mut buffer).expect("Expected to be able to read file");
+    // turn json string into wifiConfig 
+    let wifi_config : WifiConfig = serde_json::from_str(&buffer).expect("Expected Json String");
+    return wifi_config;
+}
 fn main() {
-    // Required for ESP32 Rust compatibility.
+    // Required for ESP32 Rust compatibility.    
     esp_idf_svc::sys::link_patches();
     println!("Entered Main function!");
 
@@ -22,13 +42,11 @@ fn main() {
         Some(nvs),
     ).unwrap();
 
-    let mut ssid: String<32> = String::from("");
-    let mut password: String<64> = String :: from("");
 
     // Configure WiFi with your SSID and password
     wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid,        // Update with your WiFi SSID
-        password, // Update with your WiFi Password
+        ssid:"".try_into().unwrap(),        // Update with your WiFi SSID
+        password:"".try_into().unwrap(), // Update with your WiFi Password
         ..Default::default()
     })).unwrap();
 
