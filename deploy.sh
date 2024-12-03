f58a43fb7e626e084c4a6d4c0a52aecafa73e564
Ryan McCarthy <ryanmccarthy667@gmail.com>
2024-12-03 23:19:01 +1300

    password and ssid are now parsed from wifi_config file

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+## ADD READING FROM BASH HERE ##
+SSID=$(jq .ssid wifi_config.json)
+PASSWORD=$(jq .password wifi_config.json)
+
 echo "Compiling build ..."
 cd micro_controller_code 
-cargo build --target xtensa-esp32-espidf --release
+SSID="$SSID" PASSWORD="$PASSWORD" cargo build --target xtensa-esp32-espidf --release
 echo "reseting device ..."
 espflash erase-flash
 echo "pushing new code ..."
-espflash flash --port /dev/ttyUSB0 /home/ryan/AquaInspector/AquaInspector-Sensor/micro_controller_code/target/xtensa-esp32-espidf/release/micro_controller_code \
---monitor
+espflash flash --port /dev/ttyUSB0 /home/ryan/AquaInspector/AquaInspector-Sensor/micro_controller_code/target/xtensa-esp32-espidf/release/micro_controller_code --monitor
--- micro_controller_code/src/main.rs
+++ micro_controller_code/src/main.rs
@@ -2,30 +2,12 @@ use std::{thread::sleep, time::Duration};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::wifi::{EspWifi,Configuration,ClientConfiguration};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
-use serde::Deserialize;
-use std::fs::File;
-use std::io::Read;
+
+include!(concat!(env!("OUT_DIR"), "/wifi_config.rs"));
 
 
 
-// struct to hold the wifi connection details 
-#[derive(Deserialize)]
-struct WifiConfig{
-    ssid: String,
-    password : String,
-}
 
-// function to read wifi details from file 
-fn read_wifi_config()->WifiConfig{
-    //open the file 
-    let mut file = File::open("/spiffs/wifi_config.json").expect("wifi config file should exist on device");
-    let mut buffer = String::new();
-    //read the file into a json string                                                   
-    file.read_to_string(&mut buffer).expect("Expected to be able to read file");
-    // turn json string into wifiConfig 
-    let wifi_config : WifiConfig = serde_json::from_str(&buffer).expect("Expected Json String");
-    return wifi_config;
-}
 fn main() {
     // ??? 
     esp_idf_svc::sys::link_patches();
@@ -44,18 +26,18 @@ fn main() {
     ).unwrap();
     
     // get the wifi config
-    let wifi_config = read_wifi_config();
-    // turn password and ssid into an actual string 
-    let mut ssid = heapless::String::<32>::new();
-    ssid.push_str(&wifi_config.ssid).expect("SSID too long");
+    // let wifi_config = read_wifi_config();
+    // // turn password and ssid into an actual string 
+     let mut ssid = heapless::String::<32>::new();
+     ssid.push_str(WIFI_SSID).expect("SSID too long");
 
-    let mut password = heapless::String::<64>::new();
-    password.push_str(&wifi_config.password).expect("Password too long");
+     let mut password = heapless::String::<64>::new();
+     password.push_str(WIFI_PASSWORD).expect("Password too long");
 
     // Configure WiFi with SSID and password
     wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration {
-        ssid,       
-        password, 
+        ssid:ssid,       
+        password:password, 
         ..Default::default()
     })).unwrap();
 
